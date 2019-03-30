cf3f3a7577951e9e4da5c8b0fa2eefc2f0d0fce9
Irwin Strachan <irwins@users.noreply.github.com>
2019-03-30 10:20:30 +0100

    code: Used PSScriptRoot as source Robocopy

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,3 +1,3 @@
 $fullPath = 'C:\Program Files\WindowsPowerShell\Modules\PSStringScanner'
 
-Robocopy . $fullPath /mir /XD .vscode .git examples testimonials images spikes /XF appveyor.yml .gitattributes .gitignore
+Robocopy $PSScriptRoot $fullPath /mir /XD .vscode .git examples testimonials images spikes /XF appveyor.yml .gitattributes .gitignore
