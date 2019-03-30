470c265133526d47ae1b7054e9a9d7bb002984c9
Irwin Strachan <irwins@users.noreply.github.com>
2019-03-30 12:39:01 +0100

    code: Added requires RunAsAdministrator

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,3 +1,5 @@
+#Requires -RunAsAdministrator
+
 $fullPath = 'C:\Program Files\WindowsPowerShell\Modules\PSStringScanner'
 
 Robocopy $PSScriptRoot $fullPath /mir /XD .vscode .git examples testimonials images spikes /XF appveyor.yml .gitattributes .gitignore
