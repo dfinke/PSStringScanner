6f4af20481a65616cffe8637e60e9f3cd99ea8fa
dfinke <finked@hotmail.com>
2019-04-08 19:46:10 -0400

    Added another approach

--- examples/UseScanMethodOnStringType.ps1
+++ examples/UseScanMethodOnStringType.ps1
@@ -3,7 +3,20 @@
 (Get-Content .\CountryInfo.txt).Scan("[a-zA-z ]+(?=:)")
 
 # $scanner = New-PSStringScanner (Get-Content .\CountryInfo.txt -Raw)
+
 # do {
 #     $token = $scanner.Scan("[a-zA-z ]+(?=:)")
 #     if ($token) {$token}
+# } until($null -eq $token)
+
+# do {
+#     $token = $scanner.Scan("[a-zA-z ]+(?=:)")
+#     if ($token) {
+#         $null = $scanner.Scan(":")
+#         $Amt = $scanner.ScanUntil("\r\n").trim()
+#         [PSCustomObject][Ordered]@{
+#             Country = $token
+#             Amt     = [double]::Parse($Amt)
+#         }
+#     }
 # } until($null -eq $token)
