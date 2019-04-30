090c6ed7863d94baa335ed8f58fa827686845d11
dfinke <finked@hotmail.com>
2019-04-30 12:33:55 -0400

    Try string scanner ex

---
+++ __tests__/PSStringScannerEx.tests.ps1
@@ -0,0 +1,19 @@
+$manifestPath = "$PSScriptRoot\..\PSStringScanner.psd1"
+Import-Module $manifestPath -Force
+
+Describe "ScannerEx Interface" {
+
+    It "Should work" {
+        $s = @"
+The quick brown fox 10 jumped
+over the lazy dog
+"@
+        $scanner = New-PSStringScannerEx $s
+
+        $scanner.NextWord()   | Should BeExactly "The"
+        $scanner.NextNumber() | Should Be 10
+        $scanner.NextLine()   | Should Be "`r`n"
+        $scanner.NextWord()   | Should BeExactly "over"
+        $scanner.NextWord()   | Should BeExactly "the"
+    }
+}
